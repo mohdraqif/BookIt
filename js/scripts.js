87556ba5bf99fd54978a9fad28ec185e036a7a3b
mohdraqif <mohdraqif94@gmail.com>
2019-10-13 10:44:30 +0530

    Init commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,38 @@
+var btn = document.querySelector('.nav-item')
+
+console.log(btn)
 
 function scroll_to(clicked_link, nav_height) {
-	var element_class = clicked_link.attr('href').replace('#', '.');
-	var scroll_to = 0;
+	var element_class = clicked_link.attr('href').replace('#', '.')
+	var scroll_to = 0
 	if(element_class != '.top-content') {
 		element_class += '-container';
-		scroll_to = $(element_class).offset().top - nav_height;
+		scroll_to = $(element_class).offset().top - nav_height
 	}
 	if($(window).scrollTop() != scroll_to) {
-		$('html, body').stop().animate({scrollTop: scroll_to}, 1000);
+		$('html, body').stop().animate({scrollTop: scroll_to}, 1000)
 	}
 }
 
 
 jQuery(document).ready(function() {
-	
 	/*
 	    Navigation
 	*/
 	$('a.scroll-link').on('click', function(e) {
 		e.preventDefault();
-		scroll_to($(this), $('nav').outerHeight());
-	});
+		scroll_to($(this), $('nav').outerHeight())
+	})
 	
     /*
         Background
     */
-    $('.section-4-container').backstretch("assets/img/backgrounds/bg.jpg");
+    $('.section-4-container').backstretch("assets/img/backgrounds/bg.jpg")
     
     /*
 	    Wow
 	*/
-	new WOW().init();
+	new WOW().init()
 	
 	/*
 	    Carousel
@@ -41,24 +43,23 @@ jQuery(document).ready(function() {
 	        CC 2.0 License Iatek LLC 2018
 	        Attribution required
 	    */
-	    var $e = $(e.relatedTarget);
-	    var idx = $e.index();
-	    var itemsPerSlide = 5;
-	    var totalItems = $('.carousel-item').length;
+	    var $e = $(e.relatedTarget)
+	    var idx = $e.index()
+	    var itemsPerSlide = 5
+	    var totalItems = $('.carousel-item').length
 	    
 	    if (idx >= totalItems-(itemsPerSlide-1)) {
-	        var it = itemsPerSlide - (totalItems - idx);
+	        var it = itemsPerSlide - (totalItems - idx)
 	        for (var i=0; i<it; i++) {
 	            // append slides to end
 	            if (e.direction=="left") {
-	                $('.carousel-item').eq(i).appendTo('.carousel-inner');
+	                $('.carousel-item').eq(i).appendTo('.carousel-inner')
 	            }
 	            else {
-	                $('.carousel-item').eq(0).appendTo('.carousel-inner');
+	                $('.carousel-item').eq(0).appendTo('.carousel-inner')
 	            }
 	        }
 	    }
-	});
-	
-});
+	})
+})
 
